581be65fd4250e3f028cc791322e1a9223a5f36f
M Huzaifa Alam <mhuzaifaalam7@gmail.com>
2026-09-23 14:09:39 +0500

    serializer in order is maked

---
+++ backend/orders/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'price']
+        read_only_fields = ['id']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'status', 'items', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at','updated_at']
+
+        
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order,OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
 
-# Create your views here.
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
